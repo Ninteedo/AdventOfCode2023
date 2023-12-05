1e0d10be4bf0c59d812dbaf7f7043730e0447600
Richard Holmes <richard.gordon.holmes@gmail.com>
2023-12-05 11:38:19 +0000

    Cleanup day 5 by removing title tracking, using Long instead of BigInt, and adding LongRange

--- src/main/scala/days/Day05.scala
+++ src/main/scala/days/Day05.scala
@@ -5,68 +5,49 @@ import utility.*
 class Day05 extends IDay {
   override def execute(input: String): (Any, Any) = {
     val groups: Seq[String] = input.split("\n\n")
-    val seedNumbers: Seq[BigInt] = groups.head.stripPrefix("seeds: ").split(" ").map(s => BigInt(s.strip, 10)).toSeq
+    val seedNumbers: Seq[Long] = groups.head.stripPrefix("seeds: ").split(" ").map(s => s.strip.toLong).toSeq
     val almanacEntries: Seq[AlmanacMapping] = groups.tail.map(AlmanacMapping.read)
 
     (part1(seedNumbers, almanacEntries), part2(seedNumbers, almanacEntries))
   }
 
-  def part1(seedNumbers: Seq[BigInt], almanacEntries: Seq[AlmanacMapping]): BigInt = {
-    def seedNumberToLocation(seedNumber: BigInt): BigInt =
-      almanacEntries.foldLeft(seedNumber)({(n: BigInt, almanac: AlmanacMapping) => almanac.getMappedValue(n)})
+  def part1(seedNumbers: Seq[Long], almanacEntries: Seq[AlmanacMapping]): Long = {
+    def seedNumberToLocation(seedNumber: Long): Long =
+      almanacEntries.foldLeft(seedNumber)({(n: Long, almanac: AlmanacMapping) => almanac.getMappedValue(n)})
 
     seedNumbers.map(seedNumberToLocation).min
   }
 
-  def part2(seedNumbers: Seq[BigInt], almanacEntries: Seq[AlmanacMapping]): BigInt = {
-    def seedRangeToLocationRange(seedRange: (BigInt, BigInt)): Seq[(BigInt, BigInt)] =
+  def part2(seedNumbers: Seq[Long], almanacEntries: Seq[AlmanacMapping]): Long = {
+    def seedRangeToLocationRange(seedRange: LongRange): Seq[LongRange] =
       almanacEntries.foldLeft(Seq(seedRange))({(range, almanac) => almanac.transformSeedRanges(range)})
 
-    val initialSeedRanges: Seq[(BigInt, BigInt)] =
-      (0 until seedNumbers.length / 2).map(i => (seedNumbers(2 * i), seedNumbers(2 * i + 1)))
+    val initialSeedRanges: Seq[LongRange] =
+      (0 until seedNumbers.length / 2).map(i => LongRange.fromStartAndSize(seedNumbers(2 * i), seedNumbers(2 * i + 1)))
 
-    initialSeedRanges.flatMap(seedRangeToLocationRange).map(_._1).min
+    initialSeedRanges.flatMap(seedRangeToLocationRange).map(_.start).min
   }
 
-  class AlmanacMapping(
-    val sourceName: String,
-    val destName: String,
-    val mappings: Seq[(BigInt, BigInt, BigInt)]
-  ) {
-    def getMappedValue(sourceValue: BigInt): BigInt = mappings.find((destStart, sourceStart, rangeLength) => {
-      sourceStart <= sourceValue && sourceValue < sourceStart + rangeLength
+  class AlmanacMapping(val mappings: Seq[(LongRange, Long)]) {
+    def getMappedValue(sourceValue: Long): Long = mappings.find((range, offset) => {
+      range.contains(sourceValue)
     }) match {
-      case Some((destStart, sourceStart, rangeLength)) => sourceValue + (destStart - sourceStart)
+      case Some((range, offset)) => sourceValue + offset
       case None => sourceValue
     }
 
-    def transformSeedRanges(seedRanges: Seq[(BigInt, BigInt)]): Seq[(BigInt, BigInt)] = {
-      def transformSeedRange(start: BigInt, length: BigInt): Seq[(BigInt, BigInt)] = {
-        def transformInMapping(mapping: (BigInt, BigInt, BigInt)): (BigInt, BigInt) = {
-          val (destStart, sourceStart, rangeLength) = mapping
-
-          val amountInMapping: BigInt = {
-            val overlapStart = start.max(sourceStart)
-            val overlapEnd = (start + length).min(sourceStart + rangeLength)
-            (overlapEnd - overlapStart).max(0)
-          }
-
-          (start.max(sourceStart), amountInMapping)
+    def transformSeedRanges(seedRanges: Seq[LongRange]): Seq[LongRange] = {
+      def transformSeedRange(original: LongRange): Seq[LongRange] = {
+        def transformInMapping(mapping: (LongRange, Long)): LongRange = {
+          val (range, _) = mapping
+          val amountInMapping: Long = original.overlapAmount(range)
+          LongRange.fromStartAndSize(original.start.max(range.start), amountInMapping)
         }
-        val converted: Seq[(BigInt, BigInt)] = mappings.map(transformInMapping).filter(_._2 > 0)
+        val converted: Seq[LongRange] = mappings.map(transformInMapping).filter(_.nonEmpty)
+        var remainingRange: Seq[LongRange] = List(original)  // find existing range not mapped
+        converted.foreach({ r => remainingRange = remainingRange.flatMap(_.difference(r))})
 
-        var remainingRange: Seq[(BigInt, BigInt)] = List((start, start + length))  // find existing range not mapped
-        converted.foreach({ m =>
-          val (origStart, length) = m
-          remainingRange = remainingRange.flatMap({n =>
-            val (remainingStart, remainingEnd) = n
-            val before = (remainingStart, origStart.min(remainingEnd))
-            val after = (origStart + length, remainingEnd)
-            List(before, after)
-          }).filter({p => p._1 < p._2})
-        })
-
-        converted.map(p => (getMappedValue(p._1), p._2)) ++ remainingRange.map(p => (p._1, p._2 - p._1))
+        converted.map(p => LongRange.fromStartAndSize(getMappedValue(p.start), p.size)) ++ remainingRange
       }
 
       seedRanges.flatMap(transformSeedRange)
@@ -76,17 +57,35 @@ class Day05 extends IDay {
   object AlmanacMapping {
     def read(s: String): AlmanacMapping = {
       val lines: Seq[String] = Helper.readLines(s, identity).toSeq
-      val headerLine = lines.head
-      val (sourceName, destName) = "(\\w+)-to-(\\w+) map:".r.findFirstMatchIn(headerLine) match {
-        case Some(value) => (value.group(1), value.group(2))
-      }
-
       val mappingPattern = "(\\d+) (\\d+) (\\d+)".r
-      val mappings: Seq[(BigInt, BigInt, BigInt)] = lines.tail.map(line => mappingPattern.findFirstMatchIn(line) match {
-        case Some(value) => (BigInt(value.group(1), 10), BigInt(value.group(2), 10), BigInt(value.group(3), 10))
+      val readValues: Seq[(Long, Long, Long)] = lines.tail.map(line => mappingPattern.findFirstMatchIn(line) match {
+        case Some(value) => (value.group(1).toLong, value.group(2).toLong, value.group(3).toLong)
       })
 
-      AlmanacMapping(sourceName, destName, mappings)
+      val mappings = readValues.map(t => (LongRange(t._2, t._2 + t._3), t._1 - t._2))
+      AlmanacMapping(mappings)
     }
+  }
+
+  class LongRange(val start: Long, val end: Long) {
+    def contains(value: Long): Boolean = start <= value && value < end
+
+    def difference(other: LongRange): Seq[LongRange] = {
+      val before = LongRange(start, end.min(other.start))
+      val after = LongRange(other.end, end)
+      List(before, after).filter(_.nonEmpty)
+    }
+
+    def overlapAmount(other: LongRange): Long = (end.min(other.end) - start.max(other.start)).max(0)
+
+    def nonEmpty: Boolean = start < end
+
+    val size: Long = (end - start).max(0L)
+
+    override def toString: String = s"LongRange($start, $end)"
+  }
+
+  object LongRange {
+    def fromStartAndSize(start: Long, size: Long): LongRange = LongRange(start, start + size)
   }
 }
