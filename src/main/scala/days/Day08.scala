01924c799e4d8f1138ab7e82eddb59aba084d90b
Richard Holmes <richard.gordon.holmes@gmail.com>
2023-12-08 10:03:49 +0000

    Complete day 8

---
+++ src/main/scala/days/Day08.scala
@@ -0,0 +1,62 @@
+package days
+
+import utility.*
+
+class Day08 extends IDay {
+  override def execute(input: String): (Int, Long) = {
+    val lines = input.split("\n")
+    val instructions: Array[Char] = lines.head.toCharArray
+    val connections: Map[String, (String, String)] =
+      Helper
+        .mapAllMatches("(\\w{3}) = \\((\\w{3}), (\\w{3})\\)".r, input, { m => (m.group(1), (m.group(2), m.group(3))) })
+        .toMap
+    (part1(instructions, connections), part2(instructions, connections))
+  }
+
+  def part1(instructions: Array[Char], connections: Map[String, (String, String)]): Int = {
+    var position = "AAA"
+    var steps = 0
+    while (position != "ZZZ") {
+      position = findNextPosition(connections)(position, instructions(steps % instructions.length))
+      steps += 1
+    }
+    steps
+  }
+
+  def part2(instructions: Array[Char], connections: Map[String, (String, String)]): Long = {
+    val startingNodes = connections.keys.filter(_.last == 'A')
+
+    def isEndNode(position: String): Boolean = position.last == 'Z'
+
+    var endHistory: Map[Int, Int] = Map()
+
+    var positions = startingNodes.zipWithIndex
+    var step = 0
+    while (positions.nonEmpty) {
+      positions = positions.map((pos, i) => (findNextPosition(connections)(pos, instructions(step % instructions.length)), i))
+      step += 1
+      var toRemove: List[Int] = Nil
+      positions.filter(p => isEndNode(p._1)).foreach({ (position, index) =>
+        endHistory += (index -> step)
+        toRemove = index :: toRemove
+      })
+      positions = positions.filter((_, index) => !toRemove.contains(index))
+    }
+
+    val firstEndHits: Iterable[Long] = endHistory.values.map(_.toLong)
+    firstEndHits.fold(1L)(lcm)
+  }
+
+  def findNextPosition(connections: Map[String, (String, String)])
+    (position: String, instruction: Char): String = instruction match {
+    case 'L' => connections(position)._1
+    case 'R' => connections(position)._2
+  }
+
+  def gcd(a: Long, b: Long): Long = b match {
+    case 0 => a
+    case b => gcd(b, a % b)
+  }
+
+  def lcm(a: Long, b: Long): Long = a * b / gcd(a, b)
+}
